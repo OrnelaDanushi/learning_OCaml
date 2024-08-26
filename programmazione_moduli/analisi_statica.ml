b2867421c9722bbeb192b96254b83f772fedb1d0
Ornela Danushi <orneladanushi@gmail.com>
2024-08-26 23:03:20 +0200

    Update analisi_statica.ml

--- programmazione_moduli/analisi_statica.ml
+++ programmazione_moduli/analisi_statica.ml
@@ -12,7 +12,7 @@ In maniera simile ad usare un valutatore parziale, costruire un'analisi statica.
 Sostituire il vecchio interprete da un traduttore ed un interprete.
 *)
 
-type exp = 
+type exp =                   (* vecchio interprete *)
   | Eint of int
   | Ebool of bool
   | Den of ide
@@ -31,7 +31,7 @@ type exp =
   | Appl of exp * exp list
   | Rec of ide * exp
 
-type staticexp = 
+type staticexp =             (* nuovo interprete *)
   | SUnbound
   | SInt of int
   | SBool of bool
@@ -52,10 +52,34 @@ type staticexp =
   | SRec of staticexp
 
 (*
+E' necessario ricostruire la struttura degli ambienti tramite newframes
+  per tradurre correttamente i Den ide in accessi.
+*)
 
+let newframes(e, rho) = 
+  let cframe = emptystack(cframesize(e), Expr1(e))
+  let tframe = emptystack(tframesize(e), SUnbound)
+  push(Expr1(e), cframe) 
+  push(cframe, cstack) 
+  push(tframe, tempsexpstack) 
+  pushenv(rho)
+
+(* traduttore *)
+let bind ((r: evalenv), i, d) = ...
+let bindlist (r, il, el) = ...
+let pushargs ((b: exp list), continuation) = ...
+let getargs ((b: exp list), (tempstack: staticexp stack)) = ...
+
+(* interprete *)
+let bind2 ((r: evalenv), d) = ...
+let bindlist2 (r, el) = ... 
+let pushargs2 ((b: staticexp list), continuation) = ...
+let getargs2 ((b: staticexp list), (tempstack: eval stack)) = ...
+
+
+(*
 Il traduttore valuta parzialmente il programma in input, 
   e produce un altro programma con tutti i nomi sostituiti dall'offset seguendo la catena statica.
-
 *)
 
 match top (continuation) with
@@ -96,15 +120,21 @@ match top (continuation) with
         push(Expr1(e1), continuation) 
           (* prima di chiudere la funzione in una espressione statica
           valutare il corpo in un nuovo frame*)
-        newframes(e2,bind(rho, i, SUnbound) 
+        newframes(e2,bind(rho, i, SUnbound)) 
+          (* a tempo di traduzione non c'è bisogno dei valori memorizzati nella pila evalstack
+          nel vecchio interprete si può costruire il nuovo frame con il binding solo dopo aver
+            valutato Expr(e1), cioè nella seconda fase della traduzione
+          nel nuovo interprete, quindi qui, si anticipa alla prima fase la creazione del frame,
+            inserendo nel campo valore un SUnbound
+          *)
       | Fun(i, a) ->
         let foo = List.init (List.length i) (fun _ -> Unbound)
           (*ignoriamo i valori dei parametri, usiamo una lista dummy*)
-        newframes(a,bindlist(rho, i, foo)
+        newframes(a,bindlist(rho, i, foo))
           (*prima di chiudere la funzione in una espressione statica
           valutare il corpo in un nuovo frame*)
       | Rec(f, e) ->
-        newframes(e,bind(rho, f, Unbound)
+        newframes(e,bind(rho, f, Unbound))
           (* i costrutti Rec sono sempre nella forma 
           Let (ide, (Rec (ide, Fun(... )), ...))
           Bisogna creare un nuovo frame con il binding della funzione ricorsiva,
@@ -113,18 +143,193 @@ match top (continuation) with
           Prima di fare la chiusura per il costrutto Rec (verra' fatto nel ramo Exp2)
             bisogna valutare il corpo per effettuare eventuali traduzioni.
           *)
+  | Expr2(x) ->
+    pop(continuation)
+    match x with
+      | Eint(n) -> push(SInt(n), tempstack)     (* adesso tempstack e' una pila di stacexp*)
+      | Ebool(b) -> push(SBool(b), tempstack)
+      | Den(i) -> 
+        (*
+        push(applyenv(rho, i), tempstack)  ----> tradotto in Access(int, int) 
+        la applyenv del vecchio interprete resituisce il valore alla locazione di ide
+        nel caso del problema, interessa memorizzare i due offset accesses ed index
+        non restituiamo percio' un eval ma un staticexp Access(x, y)
+        dato che non interessa il valore memorizzato, 
+          la pila evalstack risulta inutile
+        restituire una chiusura lessicale contenente gli offset nella pila 
+          dell'ambiente per prelevare il valore a runtime
+        il nuovo interprete non ha bisogno di effettuare la ricerca per nome,
+          la pila namestack diventa inutile
+        *)
+        
+        let applyenv ((x: evalenv), (y: ide)) = 
+          let n = ref(x)
+          let found = ref(false)
+          let accesses = ref 0
+          let index = ref 0
+          while not !found && !n > -1 do
+            let lenv = access(namestack, !n)
+            let n1 = Array.length lenv
+            index := 0
+            while not !found && !index < n1 do
+              if Array.get lenv !index = y then
+                found := true
+              else 
+                index := !index + 1
+              done
+              if not !found then
+                n := access(slinkstack, !n)
+                accesses := !accesses + 1
+            done
+            if !found then
+              Access(!accesses, !index)
+            else
+              SUnbound
+      | Iszero(a) -> 
+        let arg = top(tempstack)
+        pop(tempstack)
+        push(Siszero(arg), tempstack) 
+      | Eq(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(Sequ(firstarg, sndarg), tempstack) 
+      | Prod(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(Smult(firstarg, sndarg), tempstack) 
+      | Sum(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(Splus(firstarg, sndarg), tempstack) 
+      | Diff(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(Sdiff(firstarg, sndarg), tempstack) 
+      | Minus(a) -> 
+        let arg = top(tempstack)
+        pop(tempstack)
+        push(Sminus(arg), tempstack) 
+      | And(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(Set(firstarg, sndarg), tempstack) 
+      | Or(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(Svel(firstarg, sndarg), tempstack) 
+      | Not(a) -> 
+        let arg = top(tempstack)
+        pop(tempstack)
+        push(Snon(arg), tempstack) 
+      | Ifthenelse(a, b, c) -> 
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        let trdarg = top(tempstack)
+        pop(tempstack)      
+        push(Sifthenelse(firstarg, sndarg, trdarg), tempstack) 
 
+      | Appl(a, b) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(SAppl(firstarg, sndarg), tempstack) 
 
+      | Let(i, e1, e2) ->
+        let firstarg = top(tempstack)
+        pop(tempstack)
+        let sndarg = top(tempstack)
+        pop(tempstack)      
+        push(SBind(firstarg, sndarg), tempstack) 
 
+      | Fun(i, a) ->
+        let arg = top(tempstack)
+        pop(tempstack)
+        push(SFun(arg), tempstack) 
+      | Rec(f, e) ->
+        let arg = top(tempstack)
+        pop(tempstack)
+        push(SRec(arg), tempstack) 
 
+      (* una volta tradotte le sottoespressioni in ambienti con i binding effettuati correttamente,
+      e' possibile chiudere lessicalmente i costrutti
+      *)
 
+      done
+      let valore = top(top(tempsexpstack))
+      pop(top(tempsexpstack))
+      pop(cstack)
+      pop(tempsexpstack)
+      push(valore, top(tempsexpstack))
+      popenv()
+    done
+    let valore = top(top(tempsexpstack))
+    pop(tempsexpstack)
+    valore
 
+    (* al termine della traduzione, come nel vecchio interprete,
+      restituire il valore presente nella pila temporanea
+    che nel caso del problme non è un eval
+    ma una espressione statica contenente tutto l'albero di sintassi astratta del programma in input
+    dove avremo tradotto tutti i costrutti in costrutti alternativi senza ide
+    e tutti i Den(ide) in Access(int, int)
+    il codice prodotto può essere adesso dato in input al nuovo interprete
+    
+    
+    e la retention? nessuna
+    e' necessario farla quando una espressione valuta un Funval(Fun(ii, aa), r)
+    se si fa popenv l'ambiente r non esiste più al momento dell'applicazione,
+      e il valore relativo di slinkstack sarà compromesso
+    # sem(
+      Appl(Appl(Fun(["x"], Fun(["y"], Sum(Den("x", Den("y")))), [Eint 3]), [Eint 5])),
+      emptyenv Unbound);;
+
+    durante l'analisi statica interessa soltanto tradurre i nomi
+    perciò, quando si incontra un Fun(i, exp) 
+      pushiamo un frame per exp
+      e valutiamo eventuali identificatori interni
+      es: Fun(["y"], Sum(Den("x"), Den("y")))
+    quando applicheremo la funzione costruiremo semplicemente una chiusura lessicale:
+      Sappl(firstarg, sndarg)
+    l'argomento di Sappl sono le espressioni statiche con i nomi tradotti
+    non applichiamo mai, non avremo nessun problema di retention
+    *)
 
 (*
 
-Il nuovo interprete prende in input il codice valutato parzialmente,
+Il nuovo interprete prende in input il codice valutato parzialmente (valori di tipo staticexp),
   e accede direttamente alla pila di ambienti evalstack 
-  senza cercare il nome nella pila parallela namestack.
+  senza cercare il nome nella pila parallela namestack (valuta ad eval).
+Bisogna "duplicare" alcuni metodi usati dal traduttore in quanto cambiano i tipi:
+  vecchio interprete: exp -> eval
+  traduttore: exp -> staticexp
+  nuovo interpret: staticexp -> eval
+Alternativamente usare le funzioni polimorfe
+
+Soluzione scelta (più semplice): 
+2 pile 
+  cstack (di tipo exp nel traduttore, staticexp nell'interprete) 
+  tempstack (di tipo staticexp nel traduttore, eval nell'interprete) 
+duplicati i metodi 
+  bind, bindlist 
+    hanno un numero diverso di parametri, 
+    nel traduttore ci sono gli identificatori, nell'interprete no
+  pushargs, getargs (prendono exp nel traduttore, staticexp nell'interprete)
+il dominio efun adesso chiude staticexp
 
 Eseguire il programma con l'interprete sui costrutti solo dell'ambiente (ignorando tutti gli altri), 
   e cioè che guardi solo i nomi (namestack) e link (slinkstack) statici.
